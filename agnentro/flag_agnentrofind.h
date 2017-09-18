1024fc44738cc5667f65c11304da23d300510eac
Russell Leidich <egione@users.noreply.github.com>
2017-09-18 09:09:44 +0000

    Add files via upload

--- agnentro/flag_agnentrofind.h
+++ agnentro/flag_agnentrofind.h
@@ -26,4 +26,4 @@ Agnentro Find Version Info
 /*
 BUILD_ID must increase with every code release, even if only a comment changes. It allows the user to discern one build from another, but is invisible to other libraries. If this library depends on other libraries, then it should be expressed as the sum of the local build ID plus the build IDs of the dependencies, and in any event must increase monotonically even if some of the latter are removed.
 */
-#define AGNENTROFIND_BUILD_ID (4+AGNENTROPROX_BUILD_ID+ASCII_BUILD_ID+FILESYS_BUILD_ID)
+#define AGNENTROFIND_BUILD_ID (5+AGNENTROPROX_BUILD_ID+ASCII_BUILD_ID+FILESYS_BUILD_ID)
